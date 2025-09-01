94529b34cc0c830b628652d0b7585e5433a27c0f
mdrayaanpasha <mdrayaanpasha@gmail.com>
2025-09-01 09:33:25 +0530

    yea unfucking it 2.0

--- routers/auth.route.js
+++ routers/auth.route.js
@@ -3,6 +3,26 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 import nodemailer from "nodemailer";
+import dotenv from "dotenv"
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "/api/auth/google/callback"
+  },
+  (accessToken, refreshToken, profile, done) => {
+    const user = { id: profile.id, email: profile.emails?.[0].value };
+    return done(null, user);
+}
+));
+
 
 dotenv.config();
 
